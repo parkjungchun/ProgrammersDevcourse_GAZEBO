cb2aa135e758525105fa9b6063e9b29452199a62
wish-j <125460753+wish-j@users.noreply.github.com>
2023-07-07 22:18:20 +0900

    Update custom_follow_waypoints.py final

--- final_project/scripts/custom_follow_waypoints.py
+++ final_project/scripts/custom_follow_waypoints.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python3
-
+​
 import threading
 import rospy
 import actionlib
-
+​
 from smach import State, StateMachine
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import PoseArray, PoseWithCovarianceStamped
 from std_msgs.msg import Empty
-
+​
 from time import sleep
-
+​
 waypoints = []
-
+​
 def convert_PoseWithCovarianceStamped_to_PoseArray(waypoints):
     """Used to publish waypoints as pose array so that you can see them in rviz, etc."""
     poses = PoseArray()
     poses.header.frame_id = 'map'
     poses.poses = [pose.pose.pose for pose in waypoints]
-
+​
     return poses
-
+​
 class GetPath(State):
     def __init__(self):
         State.__init__(self, outcomes=['success'], input_keys=['waypoints'], output_keys=['waypoints'])
-
+​
         # Create publsher to publish waypoints as pose array so that you can see them in rviz, etc.
         self.pose_array_publisher = rospy.Publisher('waypoints', PoseArray, queue_size=1)
         self.custom_waypoint_topic = rospy.get_param('~custom_waypoints_topic', 'my_waypoints_list')
-
+​
         # Start thread to listen for reset messages to clear the waypoint queue
         def wait_for_path_reset():
             """thread worker function"""
@@ -38,36 +38,36 @@ class GetPath(State):
                 rospy.loginfo("Recieved path RESET message")
                 self.initialize_path_queue()
                 rospy.sleep(3) # Wait 3(s) for wait_for_message()
-
+​
         reset_thread = threading.Thread(target=wait_for_path_reset)
         reset_thread.start()
-
+​
     def initialize_path_queue(self):
         global waypoints
         waypoints = [] # waypoint queue
-
+​
         # publish empty waypoint queue as pose array so that you can see them the change in rviz, etc.
         self.pose_array_publisher.publish(convert_PoseWithCovarianceStamped_to_PoseArray(waypoints))
-
+​
     def execute(self, userdata):
         global waypoints
         self.initialize_path_queue()
         self.path_ready = False
-
+​
         # Start thread to listen for when the path is ready (this function will end then)
         def wait_for_path_ready(): # for thread
             """thread worker function"""
             data = rospy.wait_for_message('path_ready', Empty)
             rospy.loginfo('Recieved path READY message')
             self.path_ready = True
-
+​
         ready_thread = threading.Thread(target=wait_for_path_ready)
         ready_thread.start()
-
+​
         waypoints_topic = self.custom_waypoint_topic
         rospy.loginfo("Waiting to recieve waypoints via Pose msg on topic %s" % waypoints_topic)
         rospy.loginfo("To start following waypoints: 'rostopic pub /path_ready std_msgs/Empty -1")
-
+​
         # Wait for published waypoints
         while not self.path_ready:
             try:
@@ -77,128 +77,148 @@ class GetPath(State):
                     continue # no new waypoint within timeout, looping...
                 else:
                     raise e
-
+​
             rospy.loginfo("Recieved new point")
             waypoints.append(pose)
-
+​
             
             self.pose_array_publisher.publish(convert_PoseWithCovarianceStamped_to_PoseArray(waypoints))
-
+​
         # Path is ready! return success and move on to the next state (FOLLOW_PATH)
         return 'success'
-
+​
 ###############################################################################
-
+​
 class FollowPath(State):
     def __init__(self):
         State.__init__(self, outcomes=['success'], input_keys=['waypoints'])
         self.frame_id = rospy.get_param('~goal_frame_id', 'map')
-
+​
         # Get a move_base action client
         self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         rospy.loginfo('Connecting to move_base...')
         self.client.wait_for_server()
         rospy.loginfo('Connected to move_base.')
-
-
+​
+​
     def execute(self, userdata):
         global waypoints
-
+​
         # Execute waypoints each in sequence
         for waypoint in waypoints:
             if waypoints == []: # Break if preempted
                 rospy.loginfo("The waypoint queue has been reset.")
                 break
-
+​
             # Otherwise publish next waypoint as goal
             goal = MoveBaseGoal()
             goal.target_pose.header.frame_id = self.frame_id
             goal.target_pose.pose.position = waypoint.pose.pose.position
             goal.target_pose.pose.orientation = waypoint.pose.pose.orientation
-
-            rospy.loginfo("Executing move_base goal to position (x, y): %s, %s" %
-                          (waypoint.pose.pose.position.x, waypoint.pose.pose.position.y))
-
+​
+            # rospy.loginfo("Executing move_base goal to position (x, y): %s, %s" %
+            #               (waypoint.pose.pose.position.x, waypoint.pose.pose.position.y))
+​
             if waypoint.pose.pose.position.x == 4.741:
                 rospy.loginfo("#################################")
-                rospy.loginfo("##### Going to pick up food #####")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("##### Going to pick up food  ####")
+                rospy.loginfo("#####                        ####")
                 rospy.loginfo("#################################")
                 self.client.send_goal(goal=goal)
                 self.client.wait_for_result()
                 rospy.loginfo("#################################")
-                rospy.loginfo("####### Getting food ... ########")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("#####   Getting food ...     ####")
+                rospy.loginfo("#####                        ####")
                 rospy.loginfo("#################################")
                 sleep(5)
-
+​
             elif waypoint.pose.pose.position.x == 5.581:
                 rospy.loginfo("#################################")
-                rospy.loginfo("###### Going to serve No.3 ######")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("#####  Going to serve No.3   ####")
+                rospy.loginfo("#####                        ####")
                 rospy.loginfo("#################################")
                 self.client.send_goal(goal=goal)
                 self.client.wait_for_result()
-                rospy.loginfo("##################################")
-                rospy.loginfo("##### Serving No.3 complete  #####")
-                rospy.loginfo("##################################")
+                rospy.loginfo("#################################")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("##### Serving No.3 complete  ####")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("#################################")
                 sleep(1)
-
+​
             elif waypoint.pose.pose.position.x == -5.581:
                 rospy.loginfo("#################################")
-                rospy.loginfo("###### Going to serve No.4 ######")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("#####  Going to serve No.4   ####")
+                rospy.loginfo("#####                        ####")
                 rospy.loginfo("#################################")
                 self.client.send_goal(goal=goal)
                 self.client.wait_for_result()
-                rospy.loginfo("##################################")
-                rospy.loginfo("##### Serving No.3 complete  #####")
-                rospy.loginfo("##################################")
+                rospy.loginfo("#################################")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("##### Serving No.4 complete  ####")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("#################################")
                 sleep(1)
-
+​
             elif waypoint.pose.pose.position.x == -12.089:
                 rospy.loginfo("#################################")
-                rospy.loginfo("###### Going to serve No.5 ######")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("#####  Going to serve No.5   ####")
+                rospy.loginfo("#####                        ####")
                 rospy.loginfo("#################################")
                 self.client.send_goal(goal=goal)
                 self.client.wait_for_result()
-                rospy.loginfo("##################################")
-                rospy.loginfo("##### Serving No.3 complete  #####")
-                rospy.loginfo("##################################")
+                rospy.loginfo("#################################")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("##### Serving No.5 complete  ####")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("#################################")
                 sleep(1)
                 
             elif waypoint.pose.pose.position.x == -8.240:
-                rospy.loginfo("###############################")
-                rospy.loginfo("####### Bring the dishes ######")
-                rospy.loginfo("###############################")
+                rospy.loginfo("#################################")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("#####   Bring the dishes     ####")
+                rospy.loginfo("#####                        ####")
+                rospy.loginfo("#################################")
                 self.client.send_goal(goal=goal)
                 self.client.wait_for_result()
                 
                 
             # rospy.loginfo("To cancel the goal: 'rostopic pub -1 /move_base/cancel actionlib_msgs/GoalID -- {}'")
-
+​
             
             # self.client.send_goal(goal=goal)
             # self.client.wait_for_result()
-
+​
             
         return 'success'
-
+​
 ###############################################################################
-
+​
 class PathComplete(State):
     def __init__(self):
         State.__init__(self, outcomes=['success'])
-
+​
     def execute(self, userdata):
-        rospy.loginfo("###############################")
-        rospy.loginfo("##### Fnish bring dishes ######")
-        rospy.loginfo("###############################")
+        rospy.loginfo("#################################")
+        rospy.loginfo("#####                        ####")
+        rospy.loginfo("##### Fnish bring dishes     ####")
+        rospy.loginfo("#####                        ####")
+        rospy.loginfo("#################################")
         return 'success'
-
+​
 ###############################################################################
-
+​
 def main():
     rospy.init_node('custom_follow_waypoints')
-
+​
     sm = StateMachine(outcomes=['success'])
-
+​
     with sm:
         StateMachine.add('GET_PATH', GetPath(),
                          transitions={'success':'FOLLOW_PATH'},
@@ -208,8 +228,8 @@ def main():
                          remapping={'waypoints':'waypoints'})
         StateMachine.add('FOLLOW_COMPLETE', PathComplete(),
                          transitions={'success':'GET_PATH'})
-
+​
     outcome = sm.execute()
-
+​
 if __name__=='__main__':
     main()
